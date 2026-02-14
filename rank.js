abe322684df8f23a54e5b51d4094b07a5f74d456
Arksharma7 <arksharma269@gmail.com>
2026-02-15 04:39:52 +0530

    Add files via upload

---
+++ rank.js
@@ -0,0 +1,22 @@
+function getRank(score){
+
+if(score>=700){
+return ["God","Lift weight to be worshipped 🔱"];
+}
+else if(score>=550){
+return ["Immortal","Blood/Sweat/Injury still pushing 🦾"];
+}
+else if(score>=400){
+return ["Beast","Born to play with weights 😋"];
+}
+else if(score>=300){
+return ["Intermediate","One day u might have Ur dream build 🏄🏼‍♂️"];
+}
+else if(score>=250){
+return ["Undisciplined","Nah lets train chest on leg day 🫃🏻"];
+}
+else{
+return ["NPC","gym? 🤡🏳️‍🌈"];
+}
+
+}
